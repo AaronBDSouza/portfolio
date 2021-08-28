4506e3945afb7cfd7b2a3e735314874ebea194e7
AaronBDSouza <arrowknight007@gmail.com>
2021-08-28 23:58:22 +0530

    Updated Typing Animation Effect

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,20 @@
 import './intro.scss'
 import {DoubleArrow} from "@material-ui/icons";
+import { init } from 'ityped';
+import { useEffect,useRef } from 'react';
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay: 1800,
+            backSpeed: 60,
+            strings: ['a Software, Web & Mobile Apps Developer.', 'a Graphic Designer.', 'an AI/ML Enthusiast.'] 
+        })
+    },[])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -14,10 +27,10 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi there, I'm</h2>
                     <h1>Aaron B. D'Souza</h1>
-                    <h3>Software & Web Applications Developer</h3>
+                    <h3>I am <span className="dynamicText" ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
-                    <DoubleArrow style={{ fontSize: 75 }}/> 
+                    <DoubleArrow style={{ fontSize: 50 }}/> 
                 </a>
             </div>
         </div>
