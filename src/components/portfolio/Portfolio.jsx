468182112970f6d9e69813a518ce1dcb46d5baef
AaronBDSouza <arrowknight007@gmail.com>
2021-09-01 23:34:36 +0530

    Updated Portfolio Page Code

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,41 @@ import './portfolio.scss'
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            Portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Software</li>
+                <li>Web Applications</li>
+                <li>Mobile Apps</li>
+                <li>Graphic Designs</li>
+            </ul>
+            <div className="container">
+
+                <div className="item">
+                    <img src="assets/test.jpg" alt="portfolio1"/>
+                    <h3>Banking</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/test.jpg" alt="portfolio1"/>
+                    <h3>Banking</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/test.jpg" alt="portfolio1"/>
+                    <h3>Banking</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/test.jpg" alt="portfolio1"/>
+                    <h3>Banking</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/test.jpg" alt="portfolio1"/>
+                    <h3>Banking</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/test.jpg" alt="portfolio1"/>
+                    <h3>Banking</h3>
+                </div>                
+
+            </div>
         </div>
     )
 }
