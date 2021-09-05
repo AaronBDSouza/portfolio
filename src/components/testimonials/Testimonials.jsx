3df3a003ed397b4c6e463b565b5f4761c02779f8
AaronBDSouza <arrowknight007@gmail.com>
2021-09-05 14:58:25 +0530

    Updated Testimonials Section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,11 +5,13 @@ export default function Testimonials() {
     const testimonialsSliderData = [
         {
             id: 1,
+            name: "Alex Ferguson",
             title: "CEO of Manchester United",
             iconLeft: "https://img.freepik.com/free-psd/digital-marketing-facebook-banner-template_237398-233.jpg?size=626&ext=jpg",
             iconRight:"https://images.pexels.com/photos/170809/pexels-photo-170809.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             description: "Lorem ipsum dolor sit amet assumenda soluta, repellat, aut ratione commodi dolores quod placeat",
-            img:"https://site-cdn.givemesport.com/images/21/05/05/7379661c8dadd9c25c2573cb4992e6d0/1201.jpg"
+            img:"https://site-cdn.givemesport.com/images/21/05/05/7379661c8dadd9c25c2573cb4992e6d0/1201.jpg",
+            featured: false
         },
         {
             id: 2,
@@ -18,7 +20,8 @@ export default function Testimonials() {
             iconLeft: "https://img.freepik.com/free-psd/digital-marketing-facebook-banner-template_237398-233.jpg?size=626&ext=jpg",
             iconRight:"https://images.pexels.com/photos/170809/pexels-photo-170809.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             description: "2 Lorem ipsum dolor sit amet consectetur elit. Necessitatibus, optio assumenda molestiae soluta",
-            img:"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            img:"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            featured: true,
         },
         {
             id: 3,
@@ -27,7 +30,8 @@ export default function Testimonials() {
             iconLeft: "https://img.freepik.com/free-psd/digital-marketing-facebook-banner-template_237398-233.jpg?size=626&ext=jpg",
             iconRight:"https://images.pexels.com/photos/170809/pexels-photo-170809.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
             description: "3 Lorem ipsum dolor elit. Necessi tatibus, optio assu menda mole stiae aut ratione commodi dolores quod placeat dolor",
-            img:"https://images.pexels.com/photos/3671083/pexels-photo-3671083.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            img:"https://images.pexels.com/photos/3671083/pexels-photo-3671083.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            featured: false
         }
     ];
 
@@ -36,7 +40,7 @@ export default function Testimonials() {
             <h1>Testimonials</h1>
             <div className="container">
                 { testimonialsSliderData.map((sliderItem) =>( 
-                <div className="card">
+                <div className={ sliderItem.featured? "card featured" : "card" }>
                     <div className="top">
                         <img src={sliderItem.iconLeft} alt={sliderItem.name} className="left"/>
                         <img src={sliderItem.img} alt={sliderItem.name} className="user"/>
